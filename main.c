103e5d14a865b5be362b81f7c8128d026c781b5a
Maksim <mhamko22@student.aau.dk>
2022-11-24 12:24:40 +0100

    interface 2.0

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 
 int main()
 {
+    char departureCity[20]; // Assigning amount of characters, it did want to scan properly otherwise
+    char arrivalCity[20]; //                           --||--
+    interface(departureCity, arrivalCity); // We have added the interface function to a h file, and then we can call interface
+                                             // the same way we woulc call a strcmp or strlen
+
+
     return 0;
 }
 
