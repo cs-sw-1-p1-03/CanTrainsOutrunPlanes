4d3ab834c745c6c8c725cb99e6e597ed9b0435eb
Maksim <mhamko22@student.aau.dk>
2022-11-24 14:16:49 +0100

    Interface, input, readlist

--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 // void since using a char does not allow for multiple returns
 
 
-int main()
-{
+int main() {   /*
     char departureCity[20]; // Assigning amount of characters, it did want to scan properly otherwise
     char arrivalCity[20]; //                           --||--
     interface(departureCity, arrivalCity); // We have added the interface function to a h file, and then we can call interface
