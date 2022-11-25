809e64b608a3c6c251bcce9e9970d2f9449a0b9b
JacobStokholms <Stokholm.jacob@gmail.com>
2022-11-25 13:54:11 +0100

    Changes by wojte

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+// Standard libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include "string.h"
+//  Our libraries
 #include "distanceFunctionPlane.h"
 #include "distanceFunctionTrain.h"
 #include "interface.h"
-#include "string.h"
 #include "getListOfLists.h"
 
 
