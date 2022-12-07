3fc98362be0ff420a1d376a4ab510da5fe1dfebc
rforsb22 <113035561+rforsb22@users.noreply.github.com>
2022-12-07 12:09:55 +0100

    Changes by Maksim

--- emissionCalculator.h
+++ emissionCalculator.h
@@ -3,7 +3,7 @@
 
 #define ICLem 56 // (grams of Co2/passenger/kilometer) taken from "DSB årsrapport"
 #define ICem 47 // (grams of Co2/passenger/kilometer) taken from "DSB årsrapport"
-#define Planeem 111 // An average of (grams/Co2/passenger/kilometer) taken from "SAS årsrapport"
+#define Planeem 222 // An average of (grams/Co2/passenger/kilometer) taken from "SAS årsrapport"
 
 void co2Multiplier(route_t routes[], list_t arrayOfRoutes[],int totalRoutes) {
 
@@ -11,9 +11,11 @@ void co2Multiplier(route_t routes[], list_t arrayOfRoutes[],int totalRoutes) {
 
     for (int i = 0; i < totalRoutes; ++i) {
 
-        if (arrayOfRoutes[i].found == 1) {
-            if (strcmp(routes[i].typeOfTransport, "IC") == 0) {
-                emIC = (arrayOfRoutes[i].totalDistance * ICem);
+        if (arrayOfRoutes[i].found == 1)
+        {
+            if (strcmp(routes[i].typeOfTransport, "IC") == 0)
+            {
+                arrayOfRoutes[i].totalCO2 = (arrayOfRoutes[i].totalDistance * ICem);
                 printf("IC -> ");
                 printf("TOTAL: %.2lf grams CO2 per passenger \n", arrayOfRoutes[i].totalCO2);
             }
